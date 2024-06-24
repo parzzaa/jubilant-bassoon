dc4d0239d86378a69e44617a26744206a0978d5d
PARZA <parzaps4@gmai.com>
2024-06-24 11:17:41 +0330

    first time ?

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC = ({children}) => {
     return (
         <div className="min-h-screen bg-gray-100 p-4">
             <main className="max-w-4xl mx-auto">{children}</main>
